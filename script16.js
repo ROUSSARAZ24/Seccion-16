22d28a64d360b2300d7061275a4f4f8dde847337
ROUSSARAZ24 <frazuajev@gmail.com>
2024-10-10 15:19:19 -0500

    cc

--- script16.js
+++ script16.js
@@ -5,21 +5,7 @@ function validarFormulario(){
         return false;
         }        
         return true;
-    
-    let email=document.getElementById("email").value;
-    if(email==""){
-        alert("Todos los campos son obligatorios. ");
-        return false;
-        }        
-        return true;
-    
-        let imagendeperfil=document.getElementById("imagen").value;
-    if(imagendeperfil==""){
-        alert("Todos los campos son obligatorios. ");
-        return false;
-        }        
-        return true;
-    }
+        }
     
 
 function mostrarImagen(event){
