88f52ca3c8ec24a79942c9d6fdd3277a82fa5e78
ROUSSARAZ24 <frazuajev@gmail.com>
2024-10-10 11:43:55 -0500

    SCRIPT

--- script16.js
+++ script16.js
@@ -1,4 +1,12 @@
 
+function validarformulario(){
+    let Nombre=document.getElementById("Nombre").value;
+    let email=document.getElementById("email").value;
+    let imagen=document.getElementById("imagen").value;
+    if(Nombre=="" || email=="" || imagen==""){
+        alert("Todos los campos son obligatorios...");
+    }
+}
 function mostrarImagen(event){
     let imagen=document.getElementById('ver-imagen');
     imagen.src=URL.createObjectURL(event.target.files[0]);
