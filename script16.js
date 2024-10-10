ae7c5c62cdd67118208a741260a813358cb50606
ROUSSARAZ24 <frazuajev@gmail.com>
2024-10-10 15:51:54 -0500

    jjhh

--- script16.js
+++ script16.js
@@ -1,8 +1,8 @@
 function validarFormulario(){
-    let Nombre=document.getElementById("Nombre").value.trim();
-    let email=document.getElementById("email").value.trim();
-    let imagen=document.getElementById("imagen").value.trim();
-    if(Nombre==""|| email==""|| imagen==""){
+    let Nombre=document.getElementById("Nombre").value;
+    let email=document.getElementById("email").value;
+    let imagen=document.getElementById("imagen").value;
+    if(Nombre=="" || email=="" || imagen==""){
         alert("Todos los campos son obligatorios. ");
         return false;
         }        
