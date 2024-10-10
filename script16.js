88f4e0b7b8462b68aa18ac8b2978892a5b7fe79b
ROUSSARAZ24 <frazuajev@gmail.com>
2024-10-10 15:09:25 -0500

    dd

--- script16.js
+++ script16.js
@@ -25,6 +25,7 @@ function validarFormulario(){
         return true;
     }
     
+
 function mostrarImagen(event){
     let imagen=document.getElementById('ver-imagen');
     imagen.src=URL.createObjectURL(event.target.files[0]);
